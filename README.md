725dc0713c6f8d22cfa3936983e80a8059364866
Anders Bruvik <anders.bruvik@safespring.com>
2019-05-31 12:39:41 +0200

    created a readme, added cloud config and ip address output

--- README.md
+++ README.md
@@ -1,1 +1,42 @@
 # nextcloud-demo
+
+## how to use the demo environment:
+
+First use terraform to create the instances:
+
+It is a good idea to log into the web console first, to get an understanding of what is going on.
+
+Then do the follwing steps:
+
+Edit the file _openstack.sh_ and fill inn your username, projectname and password. Export the environment variables to your shell with:
+
+```bash
+eval `bash openstack.sh`
+```
+
+Then enter the terraform directory. You can look through the files if you want.
+
+Run terraform with:
+
+```bash
+terraform init
+terraform plan
+terraform apply
+```
+
+(you will see a lot of output from these steps)
+
+Inspect what you have created in the console
+
+In the terraform step, we created a ssh keypair. We are going to need it for later use. Export it with: 
+
+```bash
+terraform output ssh_private_key > terraform.key
+chmod 700 terraform.key
+```
+
+Now you should be able to ssh to your newly created server with:
+
+```bash
+$ ssh <ip-address-from-output> -l centos -i terraform.key
+```
--- terraform/nextcloud.tf
+++ terraform/nextcloud.tf
@@ -17,7 +17,7 @@ module "nextcloud-instance" {
     source = "./openstack-instance"
     keypair = "${module.ssh_keypair.name}"
     names = ["nextcloud"]
-    # user_data = "${file("nextcloud.conf")}"    
+    user_data = "${file("nextcloud.conf")}"    
     ssh_security_group = "${module.nextcloud-network.ssh_security_group}"
     network_id = "${module.nextcloud-network.network_id}"
     env_name = "${var.env_name}"
@@ -27,8 +27,21 @@ module "collabora-instance" {
     source = "./openstack-instance"
     keypair = "${module.ssh_keypair.name}"
     names = ["collabora"]
-    # user_data = "${file("collabora.conf")}"
+    user_data = "${file("collabora.conf")}"
     ssh_security_group = "${module.nextcloud-network.ssh_security_group}"
     network_id = "${module.nextcloud-network.network_id}"
     env_name = "${var.env_name}"
+}
+
+output "ssh_private_key" {
+    value = "${module.ssh_keypair.ssh_private_key}"
+    sensitive = true
+}
+
+output "nextcloud_address" {
+    value = "${module.nextcloud-instance.address}"
+}
+
+output "collabora_address" {
+    value = "${module.collabora-instance.address}"
 }
