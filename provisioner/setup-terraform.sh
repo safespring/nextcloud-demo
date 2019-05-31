12fb26c53ed85a2f5776ea8a6efe015c7a5d25bd
Gabriel Paues <gabriel.paues@safespring.com>
2019-05-31 11:40:54 +0000

    Added script for installing terraform

---
+++ provisioner/setup-terraform.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+sudo apt install unzip
+cd /tmp
+wget https://releases.hashicorp.com/terraform/0.12.0/terraform_0.12.0_linux_amd64.zip
+unzip terraform_0.12.0_linux_amd64.zip
+sudo cp ./terraform /usr/local/bin
